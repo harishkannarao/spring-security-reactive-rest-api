e08d4b3af978cf5cf5ec8b73135b06448bf10252
harishkannarao <harishkannarao@users.noreply.github.com>
2023-01-29 22:07:57 +0000

    Initial code from spring initializer

---
+++ src/main/java/com/harishkannarao/springsecurityreactiverestapi/SpringSecurityReactiveRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.springsecurityreactiverestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReactiveRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReactiveRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harishkannarao/springsecurityreactiverestapi/SpringSecurityReactiveRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.springsecurityreactiverestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReactiveRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
