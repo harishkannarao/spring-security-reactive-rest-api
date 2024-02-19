a08388dec050f598db6b11f469036e89f8707127
harishkannarao <harishkannarao@users.noreply.github.com>
2024-02-19 12:31:48 +0000

    Auto wiring test rest template in tests

--- src/ftIntegrationTest/java/com/harishkannarao/springsecurityreactiverestapi/configuration/AdditionalConfiguration.java
+++ src/ftIntegrationTest/java/com/harishkannarao/springsecurityreactiverestapi/configuration/AdditionalConfiguration.java
@@ -5,18 +5,9 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-
-import java.util.function.Consumer;
 
 @Configuration
 public class AdditionalConfiguration {
-
-    @Bean
-    public Consumer<ServerHttpSecurity> testEndpointCustomizer() {
-        return http -> http.authorizeExchange((auth) ->
-                auth.pathMatchers("/test-endpoint").permitAll());
-    }
 
     @Bean
     protected TestRestTemplate testRestTemplate(
--- src/integrationTest/java/com/harishkannarao/springsecurityreactiverestapi/controller/GeneralDataRestControllerIntegrationTest.java
+++ src/integrationTest/java/com/harishkannarao/springsecurityreactiverestapi/controller/GeneralDataRestControllerIntegrationTest.java
@@ -3,15 +3,24 @@ package com.harishkannarao.springsecurityreactiverestapi.controller;
 import com.harishkannarao.springsecurityreactiverestapi.AbstractBaseIntegrationTestProfile;
 import com.harishkannarao.springsecurityreactiverestapi.domain.GeneralData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GeneralDataRestControllerIntegrationTest extends AbstractBaseIntegrationTestProfile {
 
+    private final TestRestTemplate testRestTemplate;
+
+    @Autowired
+    public GeneralDataRestControllerIntegrationTest(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate;
+    }
+
     @Test
     public void test_getGeneralData() {
-        ResponseEntity<GeneralData> result = testRestTemplate().getForEntity("/general-data", GeneralData.class);
+        ResponseEntity<GeneralData> result = testRestTemplate.getForEntity("/general-data", GeneralData.class);
         assertThat(result.getStatusCode().value()).isEqualTo(200);
         GeneralData actualEntity = result.getBody();
         assertThat(actualEntity).isNotNull();
